9c979b01a9cb0566f93bc065c1d0aae42f35207c
Kutman <muctep.k@gmail.com>
2019-09-25 21:51:09 +0600

    Добавил три модели в админку.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from webapp.models import Issue, Status, Type
 
-# Register your models here.
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'summary', 'status', 'type', 'created_at']
+    list_filter = ['status', 'type']
+    list_display_links = ['pk', 'summary']
+    search_fields = ['summary', 'description']
+    fields = ['summary', 'description', 'status', 'type', 'created_at']
+    readonly_fields = ['created_at']
+
+
+admin.site.register(Issue, IssueAdmin)
+admin.site.register(Status)
+admin.site.register(Type)
