befef4f92c858be4640e043aeb51feb91549cb1e
Kutman <muctep.k@gmail.com>
2019-09-25 21:32:29 +0600

    Добавлена модель задачи

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Issue(models.Model):
+    summary = models.CharField(max_length=200, verbose_name='Заголовок')
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Описание')
+    status = models.ForeignKey('webapp.Status', on_delete=models.PROTECT, verbose_name='Статус', related_name='issues')
+    type = models.ForeignKey('webapp.Type', on_delete=models.PROTECT, verbose_name='Тип', related_name='issues')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return self.summary[:20]
+
+
+
