36d30c8822e80ec0039f5680f4a6720f28d048fc
Kutman <muctep.k@gmail.com>
2019-10-09 19:55:42 +0600

    Добавил вывод имени проекта

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -33,6 +33,7 @@ class Project(models.Model):
     def __str__(self):
         return self.name[:30]
 
+
 class Status(models.Model):
     name = models.CharField(max_length=20, verbose_name='Название')
 
