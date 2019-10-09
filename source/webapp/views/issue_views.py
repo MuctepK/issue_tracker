a3fb9f30a1adb83f10ee6e70572f2e1dbe38cd29
Kutman <muctep.k@gmail.com>
2019-10-09 19:33:57 +0600

    Веб-сайт  использует классы от Джанго при изменении и удалении задачи

--- source/webapp/views/issue_views.py
+++ source/webapp/views/issue_views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from webapp.models import Issue
 from webapp.forms import IssueForm
-from django.views.generic import TemplateView, ListView, CreateView
-from django.views import View
-from .base_views import DetailView, UpdateView, DeleteView
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView, DetailView
 
 
 class IndexView(ListView):
@@ -14,6 +12,7 @@ class IndexView(ListView):
     paginate_by = 3
     paginate_orphans = 0
     page_kwarg = 'page'
+    ordering = ['-created_at']
 
 
 class IssueView(DetailView):
@@ -38,13 +37,12 @@ class IssueUpdateView(UpdateView):
     template_name = 'update.html'
     extra_context = {'title': 'Задачи'}
 
-    def get_redirect_url(self):
+    def get_success_url(self):
         return reverse('issue_view', kwargs={'pk': self.object.pk})
 
 
 class IssueDeleteView(DeleteView):
     model = Issue
-    confirm_delete = True
     template_name = 'delete.html'
-    redirect_url = 'index'
+    success_url = reverse_lazy('index')
     extra_context = {'title': 'Задачу'}
