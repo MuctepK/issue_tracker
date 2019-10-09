088085c5237f2f65cf952b2ac2090c21f3fbf0ff
Kutman <muctep.k@gmail.com>
2019-10-09 20:33:47 +0600

    Добавлена возможность редактирования одного проекта

--- source/webapp/views/__init__.py
+++ source/webapp/views/__init__.py
@@ -1,4 +1,5 @@
 from .issue_views import IndexView, IssueView, IssueCreateView, IssueUpdateView, IssueDeleteView
 from .status_views import StatusListView, StatusCreateView, StatusDeleteView, StatusUpdateView
 from .type_views import TypeListView, TypeCreateView, TypeUpdateView, TypeDeleteView
-from .project_views import ProjectListView, ProjectCreateView, ProjectDetailView
+from .project_views import ProjectListView, ProjectCreateView, ProjectDetailView, \
+    ProjectUpdateView
--- source/webapp/views/project_views.py
+++ source/webapp/views/project_views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse, reverse_lazy
 from webapp.models import Project
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 from webapp.forms import ProjectForm
 
 
@@ -26,3 +26,13 @@ class ProjectCreateView(CreateView):
     template_name = 'create.html'
     extra_context = {'title': 'Проекта'}
     success_url = reverse_lazy('projects')
+
+
+class ProjectUpdateView(UpdateView):
+    form_class = ProjectForm
+    model = Project
+    template_name = 'update.html'
+    extra_context = {'title': 'Проекта'}
+
+    def get_success_url(self):
+        return reverse('project_view', kwargs={'pk': self.object.pk})
