d5d9a49fa8be4518fdb163a806a1e0727377fed5
Kutman <muctep.k@gmail.com>
2019-10-24 11:05:01 +0600

    Выполнен бонус

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -4,28 +4,25 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 
 
-def login_view(request):
-
+def login_view(request, **kwargs):
     context = {}
-
+    redirect_url = 'index'
     if request.method == 'POST':
-
         username = request.POST.get('username')
-
         password = request.POST.get('password')
-
+        redirect_url = request.POST.get('redirect_url')
+        print(redirect_url)
         user = authenticate(request, username=username, password=password)
-
         if user is not None:
-
             login(request, user)
-
-            return redirect('index')
-
+            return redirect(redirect_url)
         else:
-
             context['has_error'] = True
-
+    if request.method == 'GET':
+        next = request.GET.get('next')
+        if next:
+            redirect_url = next
+    context['redirect_url'] = redirect_url
     return render(request, 'login.html', context=context)
 
 
